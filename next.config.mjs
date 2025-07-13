51e10d66a40376d4e79977fc9313b7b664e8de9f
stuffsergio <sergioperezmontalvo@gmail.com>
2025-07-13 03:19:23 +0200

    prepare to prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      turbo: false, // ⚠️ Fuerza Webpack estable
-    },
+    reactStrictMode: true,
+    swcMinify: true,
   };
   
   export default nextConfig;
